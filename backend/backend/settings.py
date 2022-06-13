f649860278458d942277b817e5c6d7ce6923ad8f
Jimmy Kyalo <jimm.kyalo@gmail.com>
2022-06-13 10:39:54 +0300

    backend setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'base.apps.BaseConfig'
 ]
 
 MIDDLEWARE = [
@@ -116,7 +118,13 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'images/'
 
+STATICFILES_DIRS=[
+    BASE_DIR / 'static'
+]
+
+MEDIA_ROOT = 'static/images'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- backend/base/models.py
+++ backend/base/models.py
@@ -1,3 +1,7 @@
+from email.mime import image
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    _id = models.AutoField(primary_key=True, editable=False)
+    image= models.ImageField(null=True, blank=True,default='/placeholder.jpg')
