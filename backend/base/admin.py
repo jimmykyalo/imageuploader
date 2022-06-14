459cf3506475033d37c5e0baf43a7f94b5ec998e
Jimmy Kyalo <jimm.kyalo@gmail.com>
2022-06-14 20:44:24 +0300

    add image model to admin page

--- backend/base/admin.py
+++ backend/base/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import Image
 # Register your models here.
+class ImageAdmin(admin.ModelAdmin):
+    list_display = ('_id', 'image')
+    list_display_links = ('_id',)
+
+admin.site.register(Image, ImageAdmin)
